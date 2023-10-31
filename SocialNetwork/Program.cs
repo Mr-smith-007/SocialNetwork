de7d9b24a177bbb6cd0dd45f754821fa8584769b
Repnikov.A <repnikov.a@florcom.ru>
2023-10-31 16:57:39 +0300

    Закончен модуль 19.2

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SocialNetwork.BLL.Servises;
+using SocialNetwork.BLL.Models;
+using System;
+
+
+namespace SocialNetwork
+{
+    class Program
+    {
+        public static UserService userServise = new UserService();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в социальную сеть.");
+
+            while (true)
+            {
+                Console.Write("Для регистрации введите имя пользователя:");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Фамилия:");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Пароль:");
+                string password = Console.ReadLine();
+
+                Console.Write("Адрес Email:");
+                string email = Console.ReadLine();
+
+                var userRegistrationData = new UserRegistrationData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userServise.Register(userRegistrationData);
+                    Console.WriteLine("Регистрация прошла успешно");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.Write("Введите корректное значение");
+                }
+                catch (Exception)
+                {
+                    Console.Write("Произошла ошибка при регистрации");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
